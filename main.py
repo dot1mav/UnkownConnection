3c7007685a14b945034cd1d9fc1d6c39de2cf69b
ma v <dr98mav@gmail.com>
2020-10-24 12:31:24 +0330

    get proxy

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import bs4
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def get_proxies():
+    site = 'https://free-proxy-list.net/'
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = requests(site, headers=hdr)
+    html = BeautifulSoup(req, "lxml")
+    rows = html.findAll("tr")
+    proxies = []
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text for ele in cols]
+        try:
+            ipaddr = cols[0]  # ipAddress
+            portNum = cols[1]  # portNum
+            proxy = ipaddr+":"+portNum  # concatinating
+            portName = cols[6]  # portName variable yes / No
+            if portName == "no":
+                pass  # proxies.append(str(proxy))
+            else:
+                # if portNum=='80' or portNum=='8080':
+                proxies.append(str(proxy))
+        except:
+            pass
+    return proxies
+
+
+if __name__ == "__main__":
+    proxy_list: list = get_proxies()
+    for proxy in proxy_list:
+        print(f'{proxy}')
