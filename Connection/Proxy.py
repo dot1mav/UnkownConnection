a9e3c19efb5faa1a5aeae14a771aa7a5e7998c2e
ma v <dr98mav@gmail.com>
2020-10-25 11:44:27 +0330

    add object methods

--- Connection/Proxy.py
+++ Connection/Proxy.py
@@ -4,15 +4,34 @@ from bs4 import BeautifulSoup
 
 
 class Proxy(object):
+    # attributes
+    __doc__ = " This class made to build and check and change proxy list "
+    __proxies: dict = dict()
 
-  # attributes
-  __doc__ = " This class made to build and check and change proxy list "
-  __proxies: dict = dict()
-
-   def __init__(self) -> None:
+    def __init__(self) -> None:
         pass
 
-    # protected methods
+        # object methods
+
+    def __call__(self, *args, **kwargs) -> dict:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def __del__(self) -> None:
+        pass
+
+    def __sizeof__(self) -> int:
+        pass
+
+    def __iter__(self) -> dict:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+        # protected methods
 
     def _GetProxy() -> dict:
         pass
@@ -20,7 +39,7 @@ class Proxy(object):
     def _ChangeProxy() -> dict:
         pass
 
-    # private mthods
+        # private mthods
 
     def __CheckConnection(host: str) -> bool:
         pass
