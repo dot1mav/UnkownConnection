3aecd60caddfdb208651c9fd60324f5944be9090
ma v <dr98mav@gmail.com>
2020-10-25 11:34:55 +0330

    add methods

--- Connection/Proxy.py
+++ Connection/Proxy.py
@@ -1,4 +1,26 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
 class Proxy(object):
-    
-    def __init__(self) -> None:
+
+  # attributes
+  __doc__ = " This class made to build and check and change proxy list "
+  __proxies: dict = dict()
+
+   def __init__(self) -> None:
+        pass
+
+    # protected methods
+
+    def _GetProxy() -> dict:
+        pass
+
+    def _ChangeProxy() -> dict:
+        pass
+
+    # private mthods
+
+    def __CheckConnection(host: str) -> bool:
         pass
