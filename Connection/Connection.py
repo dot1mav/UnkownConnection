a33a0bda6ace11fcef34575b9e2160fbdaa7818a
ma v <dr98mav@gmail.com>
2020-11-13 20:05:54 +0330

    make it one proxy

--- Connection/Connection.py
+++ Connection/Connection.py
@@ -9,9 +9,7 @@ from .Proxy import ProxyList
 
 class Connection(object):
     __local_ip: str = None
-    __proxy_1: str = None
-    __proxy_2: str = None
-    __proxy_3: str = None
+    __proxy: str = None
 
     def __init__(self) -> None:
         self._proxy: ProxyList = ProxyList()
@@ -23,6 +21,4 @@ class Connection(object):
 
     def __str__(self) -> str:
         return f'''local -> {self.__local_ip}
-proxy number 1 -> {self.__proxy_1}
-proxy number 2 -> {self.__proxy_2}
-proxy number 3 -> {self.__proxy_3}'''
+proxy number 1 -> {self.__proxy}'''
